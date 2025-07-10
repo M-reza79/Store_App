16ec536bdbbfe968d90fcda38636a2985846fa84
M_reza <m.reza79me@gmail.com>
2025-07-10 12:37:39 +0330

    add screen daste bandi

--- lib/pages/category_screen.dart
+++ lib/pages/category_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:store_app/constants/colors.dart';
 
-class CategoryScreen extends StatelessWidget {
+class CategoryScreen
+    extends StatelessWidget {
   const CategoryScreen({super.key});
 
   @override
@@ -10,9 +11,95 @@ class CategoryScreen extends StatelessWidget {
       backgroundColor:
           Range.backgroundScreenColor,
       body: SafeArea(
-        child: Container(
-          color: Colors.amber,
-          child: Text('dasd'),
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Padding(
+                padding:
+                    const EdgeInsets.only(
+                      top: 5,
+                      right: 30,
+                      left: 30,
+                      bottom: 30,
+                    ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.all(
+                          Radius.circular(
+                            15,
+                          ),
+                        ),
+                  ),
+                  height: 46,
+                  child: Row(
+                    crossAxisAlignment:
+                        CrossAxisAlignment
+                            .center,
+                    children: [
+                      SizedBox(width: 16),
+                      Image.asset(
+                        'assets/images/icon_apple_blue.png',
+                      ),
+
+                      Expanded(
+                        child: Text(
+                          'دسته بندی',
+                          textAlign:
+                              TextAlign
+                                  .center,
+                          style: TextStyle(
+                            fontFamily: 'SB',
+                            fontSize: 16,
+                            color: Range
+                                .blueIndicator,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SliverPadding(
+              padding: EdgeInsets.only(
+                bottom: 100,
+                right: 30,
+                left: 30,
+              ),
+              sliver: SliverGrid(
+                delegate: SliverChildBuilderDelegate((
+                  context,
+                  index,
+                ) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: Range.red,
+                      borderRadius:
+                          BorderRadius.all(
+                            Radius.circular(
+                              15,
+                            ),
+                          ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Item $index',
+                      ),
+                    ),
+                  );
+                }, childCount: 10),
+                gridDelegate:
+                    SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 1,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 20,
+                    ),
+              ),
+            ),
+          ],
         ),
       ),
     );
